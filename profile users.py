f28ded159cdb45b7a1be13b32356060955b58dee
spriso <Nasseralbadry333@gmail.com>
2025-07-13 03:01:26 +0300

    Add files via upload

---
+++ profile users.py
@@ -0,0 +1,37 @@
+import sqlite3 as bd
+
+import random as rn
+
+
+from faker import Faker
+
+import time 
+
+f=Faker()
+num=1
+start=time.time()
+
+cx=bd.connect('file.bd')
+cu=cx.execute('create table if not exists users(numb integer,id integer, name text,email text,age integer)')
+
+start1=time.time()
+
+for i in range(1001):
+    name=f.name()
+    email=f.email()
+    id=rn.randint(111111111,999999999)
+    age=rn.randint(18,59)
+    cu.execute(f'insert into users(numb,id,name,email,age) values({num},{id},"{name}","{email}",{age})')
+    end=time.time()
+    num+=1
+    
+print(f'time start {start1-start} and end time {end-start}')    
+cx.commit()    
+cx.close()
+    
+                          
+                      
+                      
+                 
+            
+    
